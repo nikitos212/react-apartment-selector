f2ec6f388a5852b8d0e635588de43f5c7582d7d1
nikitos212 <nikita.08t07@gmail.com>
2026-05-03 18:30:33 +0300

    style: use figma trash icon asset

--- src/components/IconButton/IconButton.jsx
+++ src/components/IconButton/IconButton.jsx
@@ -8,13 +8,7 @@ const ICONS = {
     </svg>
   ),
   trash: (
-    <svg viewBox="0 0 20 20" aria-hidden="true" focusable="false">
-      <path d="M3.75 5.25h12.5" />
-      <path d="M8 5.25V3.75h4v1.5" />
-      <path d="M5.5 5.25l.65 11h7.7l.65-11" />
-      <path d="M8.5 8.25v5.5" />
-      <path d="M11.5 8.25v5.5" />
-    </svg>
+    <img src="/trash.png" alt="" aria-hidden="true" draggable="false" />
   ),
   close: (
     <svg viewBox="0 0 20 20" aria-hidden="true" focusable="false">
